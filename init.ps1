adaf6156d372155ea2f979fce7184277f3db7858
Filipe Miguel Pinho Santos <fm.santos@sihot.com>
2025-05-11 02:24:02 +0100

    feat: Adjust to windows

--- init.ps1
+++ init.ps1
@@ -1,2 +1,2 @@
-Copy-Item -Path "$env:USERPROFILE\.config\wezterm\wezterm-sessionizer.sh" -Destination "$env:USERPROFILE\wezterm-sessionizer.sh"
+Copy-Item -Path "$env:USERPROFILE\.config\wezterm\sessionizer.lua" -Destination "$env:USERPROFILE\"
 Copy-Item -Path "$env:USERPROFILE\.config\wezterm\.wezterm.lua" -Destination "$env:USERPROFILE\"
