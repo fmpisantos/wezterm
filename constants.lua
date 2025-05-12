4c211cfaa969536ef8cc7d09357ada188d1be8c7
Filipe Miguel Pinho Santos <fm.santos@sihot.com>
2025-05-12 11:07:16 +0100

    feat: Adjusted constants.directories

--- constants.lua
+++ constants.lua
@@ -1,13 +1,15 @@
 local M = {}
 
 M.directories = {
-    { "D:\\",                           0 },
-    { "D:\\src",                        0 },
-    { "D:\\SIHOT.PMS",                  0 },
-    { "D:\\net-nuget-packages",         0 },
-    { "~\\.config",                     0 },
-    { "~\\Documents/WindowsPowerShell", 0 },
+    { "D:\\",                                   0 },
+    { "D:\\src",                                0 },
+    { "D:\\SIHOT.PMS",                          0 },
+    { "D:\\SIHOT.PMS",                          0 },
+    { "D:\\sihot-guest-journey",                0 },
+    { "C:\\Users\\fm.santos\\Documents\\notes", 0 },
+    { "D:\\net-nuget-packages",                 0 },
+    { "~\\.config",                             0 },
+    { "~\\Documents/WindowsPowerShell",         0 },
 }
 
 return M
-
