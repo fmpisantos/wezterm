2e9514bcfee3d62833bd2d1a4be33609a2696a25
Filipe Miguel Pinho Santos <fm.santos@sihot.com>
2025-05-17 21:02:52 +0100

    dev: WIP

--- constants.lua
+++ constants.lua
@@ -4,7 +4,6 @@ M.directories = {
     { "D:\\",                                   0 },
     { "D:\\src",                                0 },
     { "D:\\SIHOT.PMS",                          0 },
-    { "D:\\SIHOT.PMS",                          0 },
     { "D:\\sihot-guest-journey",                0 },
     { "C:\\Users\\fm.santos\\Documents\\notes", 0 },
     { "D:\\net-nuget-packages",                 0 },
