e6488e8103ef5d7553b4c6aa14e10573bd570bb0
Filipe Santos <filipesantosdev@gmail.com>
2025-05-18 21:45:53 +0100

    feat: dev

--- init.sh
+++ init.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-# cp ~/.config/wezterm/.wezterm.lua ~/
-# cp ~/.config/wezterm/sessionizer.lua ~/
 ln -s ~/.config/wezterm/.wezterm.lua ~/.wezterm.lua
 ln -s ~/.config/wezterm/sessionizer.lua ~/sessionizer.lua
 ln -s ~/.config/wezterm/constants.lua ~/constants.lua
